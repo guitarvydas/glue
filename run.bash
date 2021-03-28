7d87c823e85a3bc58a511f15147c38b4d7dbdd7d
Paul Tarvydas <guitarvydas@gmail.com>
2021-03-28 16:50:25 -0400

    upgraded run.bash

--- run.bash
+++ run.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
-node glue <semantics.glue
+
+#node glue <../svgtranspile/svg2p.glue >_svg2p-sem.js
+#diff -w _svg2p-sem.js ../svgtranspile/_temp.js
+
+node glue <glue.glue >gen-glue-sem.js
+cat gen-glue1.js gen-glue-sem.js gen-glue2.js >gen-glue.js
+node gen-glue.js <$1
